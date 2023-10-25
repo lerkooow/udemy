70c1a79eed0077c52ab3db397d523ecce6f74e9e
Valeriia <lerkoooow@gmail.com>
2023-10-25 13:47:24 +0700

    complete exercise 1

--- project1/script.js
+++ project1/script.js
@@ -14,7 +14,7 @@
     - 'Один из последних просмотренных фильмов?'
     - 'На сколько оцените его?'
 Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
+Записать ответы в объект movies в формате:
     movies: {
         'logan': '8.1'
     }
@@ -23,3 +23,28 @@
 
 'use strict';
 
+// 1
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+// 2
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+// 3
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = +prompt("На сколько оцените его?", ""),
+    c = prompt("Один из последних просмотренных фильмов?", ""),
+    d = +prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
